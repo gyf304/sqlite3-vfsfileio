f47562c9bcd6482ab59828f8ae6f494efd569d8d
Yifan Gu <gyf304@gmail.com>
2024-01-07 02:08:13 -0500

    add license

--- vfsfileio.c
+++ vfsfileio.c
@@ -1,3 +1,15 @@
+/*
+** 2024-01-07
+**
+** The author disclaims copyright to this source code.  In place of
+** a legal notice, here is a blessing:
+**
+**    May you do good and not evil.
+**    May you find forgiveness for yourself and forgive others.
+**    May you share freely, never taking more than you give.
+**
+*/
+
 #include <stddef.h>
 #include "sqlite3ext.h"
 SQLITE_EXTENSION_INIT1
